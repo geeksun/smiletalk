1eb16b8f92fd05cab69394809983d1546a00b6e2
jzq1999 <jzq1999@8073872e-c9ed-11de-96c6-3ffad103d6f9>
2011-01-11 06:49:18 +0000

    包名改变，增加日志拦截器

---
+++ smiletalk/bird/src/com/bird/interceptor/AuthenticationInterceptor.java
@@ -0,0 +1,36 @@
+package com.bird.interceptor;
+
+import com.bird.domain.UserBean;
+import com.bird.util.ConstantUtil;
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+import java.util.Map;
+
+/**
+ * @author geeksun
+ * @version : Authentication Interceptor(验证是否登录)
+ *  2009-12-3
+ */
+public class AuthenticationInterceptor extends AbstractInterceptor {
+
+	private static final long serialVersionUID = -5632216855718494698L;
+	
+	/**
+	 *  研究下 ActionInvocation 用途
+	 */
+	public String intercept(ActionInvocation actionInvocation) throws Exception {
+		Map session = actionInvocation.getInvocationContext().getSession();
+		UserBean userBean = (UserBean) session.get(ConstantUtil.USERNAME);
+		if (userBean == null) {
+			return Action.LOGIN;
+		} else {
+			Action action = (Action) actionInvocation.getAction();
+			return Action.ERROR;
+		}
+	}
+	
+	
+	
+}
---
+++ smiletalk/bird/src/com/bird/interceptor/LogInterceptor.java
@@ -0,0 +1,104 @@
+package com.bird.interceptor;
+
+import java.net.URLDecoder;
+import java.util.Date;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.ambow.log.db.DataBaseUtil;
+import com.ambow.log.timer.LogProcess;
+import com.ambow.log.util.LogUtil;
+import com.ambow.log.vo.ActionModuleVo;
+import com.ambow.log.vo.OperateLogVo;
+import com.bird.domain.UserBean;
+import com.bird.util.ConstantUtil;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+@SuppressWarnings("serial")
+public class LogInterceptor extends AbstractInterceptor {
+
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		HttpServletRequest request = ServletActionContext.getRequest();
+		String method = invocation.getProxy().getMethod();
+		
+		ActionContext ctx = invocation.getInvocationContext();
+		Map session = ctx.getSession();
+		
+		final Map<String, Object> parameters = invocation
+        .getInvocationContext().getParameters();
+		StringBuilder sb_key = new StringBuilder();
+		StringBuilder sb_value = new StringBuilder();
+	    for(Map.Entry<String,Object> entry:parameters.entrySet()){
+        	String key = (String) entry.getKey();
+        	// check key
+        	if(LogUtil.isAvailable(key)){
+            	//sb.append("key:");
+        		sb_key.append(key);
+        		sb_key.append("，");
+            	
+            	String[] values = (String[]) entry.getValue();
+            	//sb.append("value:");
+            	for(String value:values){
+            		sb_value.append(value);
+            		sb_value.append("，");
+            	}
+        	}
+        }
+	    String parameterStr = sb_key.toString();
+	    if(!LogUtil.isBlank(parameterStr)){
+	    	parameterStr = parameterStr.substring(0, parameterStr.length()-1);
+	    	parameterStr = URLDecoder.decode(parameterStr, "UTF-8");
+	    }
+	    String valueStr = sb_value.toString();
+	    if(!LogUtil.isBlank(valueStr)){
+	    	valueStr = valueStr.substring(0, valueStr.length()-1);
+	    	valueStr = URLDecoder.decode(valueStr, "UTF-8");
+	    }
+	    
+	    String ip = request.getRemoteAddr();
+	    
+	    // 判断用户是否已登录
+		UserBean user = (UserBean) session.get(ConstantUtil.USER);
+		
+        if (user != null) {
+        	String url = request.getRequestURI();
+        	ActionModuleVo actionModuleVo = DataBaseUtil.findActionModule(url);
+        	if(actionModuleVo!=null){		
+        		// operate log
+        		OperateLogVo operLog = new OperateLogVo();
+        		operLog.setOptId(String.valueOf(user.getUserId()));
+        		StringBuilder logContent = new StringBuilder();
+        		logContent.append("用户");
+        		logContent.append(user.getUserName());
+        		logContent.append("访问模块\"");
+        		logContent.append(actionModuleVo.getActionCn());
+        		logContent.append("\"，访问的URL是\"");
+        		logContent.append(url);
+        		logContent.append("\"，方法是\"");
+        		logContent.append(method);
+        		logContent.append("\"，参数\"");
+        		logContent.append(parameterStr);
+        		logContent.append("\"，参数值\"");
+        		logContent.append(valueStr);
+        		logContent.append("\"");
+        		operLog.setOptContent(logContent.toString());
+        		operLog.setOptTime(new Date());
+        		operLog.setObjId(parameterStr);
+            	operLog.setIpAddress(ip);
+            	operLog.setUrl(url);
+            	operLog.setModuleId(actionModuleVo.getModuleId());
+            	
+            	LogProcess.logList.add(operLog);
+        	}
+        }
+        
+        return invocation.invoke();
+	}
+
+}
